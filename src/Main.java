ecd99f3579521c2b9381d3e1d8abad8331a3cc21
juancarlos200 <jk6610687@gmai.com>
2026-02-02 11:07:55 -0600

    dsds

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,31 @@ public class Main {
         try {
            
             // Scanner permitirá leerlo línea por línea
-            Scanner archivo = new Scanner(new File("datos.txt"));
+            Scanner documento = new Scanner(new File("datos.txt"));
 
             // Contador para numerar las expresiones que se van leyendo
-            int numeroLinea = 1;
+            int LineaCodigo = 1;
 
 
-            System.out.println("--- Calculadora ---\n");
+            
 
-            // Este ciclo se ejecuta mientras existan líneas en el archivo
-            while (archivo.hasNextLine()) {
+            // Este ciclo se ejecuta mientras existan líneas en el documento
+            while (documento.hasNextLine()) {
 
-                // Lee una línea completa y elimina espacios al inicio y final
-                String linea = archivo.nextLine().trim();
+                // Lee una línea completa y elimina s al inicio y final
+                String espacio = documento.nextLine().trim();
 
                 
-                if (linea.isEmpty()) {
+                if (espacio.isEmpty()) {
                     continue;
                 }
 
                 // Muestra la expresión que se va a evaluar
-                System.out.print("Expresión " + numeroLinea + ": " + linea + " = ");
+                System.out.print("Expresión " + LineaCodigo + ": " + espacio + " = ");
 
                 try {
                     // Se envía la línea a la calculadora postfix para evaluarla
-                    int resultado = calculadora.evaluar(linea);
+                    int resultado = calculadora.evaluar(espacio);
 
                   
                     System.out.println(resultado);
@@ -44,15 +44,15 @@ public class Main {
                 }
 
                 // Aumenta el contador de número de expresión
-                numeroLinea++;
+                LineaCodigo++;
             }
 
-            // Se cierra el archivo 
-            archivo.close();
+            // Se cierra el documento 
+            documento.close();
 
         } catch (FileNotFoundException e) {
-            // Este error ocurre si no se encuentra el archivo txt
-            System.err.println("No se logro identificar el archivo 'datos.txt'");
+            // Este error ocurre si no se encuentra el documento txt
+            System.err.println("No se a logro identificar el documento 'datos.txt'"+e);
         }
     }
 }
