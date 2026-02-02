ced9a3ad1a8bcce0ceb3ec82b43f2426879033d8
juancarlos200 <jk6610687@gmai.com>
2026-02-02 00:11:31 -0600

    comnom

--- src/VPila.java
+++ src/VPila.java
@@ -3,25 +3,25 @@ import java.util.ArrayList;
 public class VPila<T> implements Pila<T> {
 
     ArrayList<T> elementos = new ArrayList<>();
-
+// agregar elemento a el top de la pila
     @Override
     public void push(T elemento) {
         elementos.add(elemento);
     }
-
+//obtiene el ultimo elemento de la pila y lo elimina
     @Override
     public T pop() {
         T valor = elementos.get(elementos.size() - 1);
         elementos.remove(elementos.size() - 1);
         return valor;
     }
-
+//Obtiene ultimo elemento sin eliminar 
     @Override
     public T peek() {
         T valor = elementos.get(elementos.size() - 1);
         return valor;
     }
-
+// ver si la pila esta vacia
     @Override
     public boolean isEmpty() {
         if (elementos.size() == 0) {
@@ -30,7 +30,7 @@ public class VPila<T> implements Pila<T> {
             return false;
         }
     }
-
+//cantidad elementos en pila
     @Override
     public int size() {
         int cantidad = elementos.size();
